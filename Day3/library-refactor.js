74fc53c1706e8ef1690d77b1b01cb92a365c7c50
HaoJiang0201 <haojiang0201@gmail.com>
2019-04-12 18:18:47 +0000

    library-refactor.js

--- Day3/library-refactor.js
+++ Day3/library-refactor.js
@@ -1,27 +1,38 @@
 var library = {
-  // Tracks and Play Lists Database
-  tracks: { t01: { id: "t01",
-                   name: "Code Monkey",
-                   artist: "Jonathan Coulton",
-                   album: "Thing a Week Three" },
-            t02: { id: "t02",
-                   name: "Model View Controller",
-                   artist: "James Dempsey",
-                   album: "WWDC 2003"},
-            t03: { id: "t03",
-                   name: "Four Thirty-Three",
-                   artist: "John Cage",
-                   album: "Woodstock 1952"}
-          },
-  playlists: { p01: { id: "p01",
-                      name: "Coding Music",
-                      tracks: ["t01", "t02"]
-                    },
-               p02: { id: "p02",
-                      name: "Other Playlist",
-                      tracks: ["t03"]
-                    }
-             },
+  // Tracks Database
+  tracks: {
+    t01: {
+      id: "t01",
+      name: "Code Monkey",
+      artist: "Jonathan Coulton",
+      album: "Thing a Week Three"
+    },
+    t02: {
+      id: "t02",
+      name: "Model View Controller",
+      artist: "James Dempsey",
+      album: "WWDC 2003"
+    },
+    t03: {
+      id: "t03",
+      name: "Four Thirty-Three",
+      artist: "John Cage",
+      album: "Woodstock 1952"
+    }
+  },
+  // Play Lists Database
+  playlists: {
+    p01: {
+      id: "p01",
+      name: "Coding Music",
+      tracks: ["t01", "t02"]
+    },
+    p02: {
+      id: "p02",
+      name: "Other Playlist",
+      tracks: ["t03"]
+    }
+  },
   // FUNCTIONS TO IMPLEMENT:
 
   // prints a list of all playlists, in the form:
@@ -192,7 +203,7 @@ var library = {
 
 //Tested Funcions
 
-//library.printPlaylists();
+library.printPlaylists();
 
 //library.printTracks();
 
